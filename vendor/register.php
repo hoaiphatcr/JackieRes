e55b5fbcc89d5cceee0aebd0a3bca69c5301261e
Phat Nguyen <T161968@vanlanguni.vn>
2018-05-22 10:04:33 +0700

    Add Bootstrap

---
+++ vendor/register.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php   session_start();  ?>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SB Admin - Start Bootstrap Template</title>
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body class="bg-dark">
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">Login</div>
+      <div class="card-body">
+        <form action="" method="POST">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input class="form-control" id="exampleInputEmail1" type="text" aria-describedby="emailHelp" placeholder="Enter email" name="username">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" name="password">
+          </div>
+          <div class="form-group">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember Password</label>
+            </div>
+          </div>
+          <input class="btn btn-primary btn-block" type="submit" value="Login" name="login">
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="register.php">Register an Account</a>
+          <a class="d-block small" href="forgot-password.php">Forgot Password?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php
+// insert code PHP here
+require "database-config.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if( isset( $_POST["login"]) ) {
+    $_user = $_POST['username'];
+    $_pass = $_POST['password'];
+    // echo "<p> $_user $_pass </p>";
+   	$sql = "SELECT * FROM users WHERE username = '$_user' and password = '$_pass' ";
+
+   	$result = mysqli_query($conn,$sql);
+      
+    $count = mysqli_num_rows($result);
+
+    if($count == 1) {
+        $_SESSION["login_status"]= "ready";
+        $_SESSION["name"]= "Jackie Sama";
+         
+        header("Location: index.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   
+  }
+ 
+}
+
+?>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+</body>
+</html>
